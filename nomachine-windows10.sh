9c26749174c1b1f40fb7e352411c7b08f7ed43a8
rdpddddd <155715039+rdpddddd@users.noreply.github.com>
2024-07-26 09:53:35 +0300

    Update nomachine-windows10.sh

--- nomachine-windows10.sh
+++ nomachine-windows10.sh
@@ -18,6 +18,7 @@ clear
 echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
 read -p "Paste Ngrok Authtoken: " CRP
 ./ngrok config add-authtoken $CRP 
+
 clear
 echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
 echo "======================="
@@ -34,7 +35,7 @@ read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:wine
 clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo Done! NoMachine Information:
@@ -43,4 +44,4 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo User: user
 echo Passwd: 123456
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
-seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
+seq 1 31557600 | while read i; do echo -en "\r Running .     $i s /31557600 s";sleep 0.1;echo -en "\r Running ..    $i s /31557600 s";sleep 0.1;echo -en "\r Running ...   $i s /31557600 s";sleep 0.1;echo -en "\r Running ....  $i s /31557600 s";sleep 0.1;echo -en "\r Running ..... $i s /31557600 s";sleep 0.1;echo -en "\r Running     . $i s /31557600 s";sleep 0.1;echo -en "\r Running  .... $i s /31557600 s";sleep 0.1;echo -en "\r Running   ... $i s /31557600 s";sleep 0.1;echo -en "\r Running    .. $i s /31557600 s";sleep 0.1;echo -en "\r Running     . $i s /31557600 s";sleep 0.1; done
